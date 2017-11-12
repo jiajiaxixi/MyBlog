ab2d27bc26c7bd7953c7ea219a2653c6e85e7205
Jiaxi Zhao <zhaojiaxi666@gmail.com>
2017-11-12 17:33:17 +0000

    first commit

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ var commentRoutes = require("./routes/comments");
 var campgroundRoutes = require("./routes/campgrounds");
 var indexRoutes = require("./routes/index");
 
-mongoose.connect("mongodb://localhost/yelp_camp_v12", {useMongoClient: true});
+mongoose.connect("mongodb://localhost/yelp_camp_v13", {useMongoClient: true});
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs"); 
 app.use(express.static(__dirname + "/public"));
